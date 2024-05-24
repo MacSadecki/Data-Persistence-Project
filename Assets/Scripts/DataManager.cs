9a882baf1e090b6787a56dc442c041fb09a2f14a
Maciej Sadecki <mac.sadecki@gmail.com>
2024-05-24 17:30:05 +0200

    Add save and load
    Added saving and loading of highscore value and name of the player that achieved it.

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
     public string playerName;
+
+    [Header("High Score")]
+    public string highScoreName;
+    public int highScoreValue;
+    
 
     #region Instance
 
@@ -25,10 +31,53 @@ public class DataManager : MonoBehaviour
 
     #endregion  
     
+    private void Start() 
+    {
+        LoadScore();
+    }
+
     public void SetPlayerName(string name)
     {
         playerName = name;
     }
 
+    public void SetHighScore(string name, int value)
+    {
+        highScoreName = name;
+        highScoreValue = value;
+        SaveScore();
+    }
+
+    private void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.playerName = highScoreName;
+        data.highScore = highScoreValue;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    private void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highScoreName = data.playerName;
+            highScoreValue = data.highScore;
+        }
+    }
+
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int highScore;
+    }
 
 }
